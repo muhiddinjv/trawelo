c04f1129deca8096acf40e41fc64e70678c22e0b
Emuallim <muhiddin001@gmail.com>
2021-12-05 11:42:31 +0500

    refactor: move port to seperate file

--- src/server/server.js
+++ src/server/server.js
@@ -28,12 +28,4 @@ app.post('/apis', async (req, res) => {
 	}
 });
 
-const port = process.env.PORT || 1010;
-
-// designates what port the app will listen to for incoming requests
-app.listen(port, (error) => {
-  if (error) throw new Error(error);
-  console.log(`Travel app listening on port ${port}!`);
-});
-
 module.exports = app;
