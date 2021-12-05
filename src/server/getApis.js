bd9963f69106cc50f4f4760c3d08f03c272a9cdc
Emuallim <muhiddin001@gmail.com>
2021-12-05 12:12:01 +0500

    fix: This page isn’t working

--- src/server/getApis.js
+++ src/server/getApis.js
@@ -3,18 +3,17 @@ const axios = require('axios');
 const GEO_URL = 'http://api.geonames.org/searchJSON?q';
 const WB_URL = 'https://api.weatherbit.io/v2.0/forecast/daily?';
 const PIX_URL = 'https://pixabay.com/api/?';
-const { GEO_USER, WB_KEY, PIX_KEY } = process.env;
 
 const getApis = async (cityName) => {
   try {
-    const geoData = await axios.get(`${GEO_URL}=${cityName}&maxRows=1&username=${GEO_USER}`);
+    const geoData = await axios.get(`${GEO_URL}=${cityName}&maxRows=1&username=${process.env.GEO_USER}`);
 
     const { name, countryName, lat, lng } = geoData.data.geonames[0];
 
     const [weatherData, pixData1, pixData2] = await Promise.all([
-      axios.get(`${WB_URL}&lat=${lat}&lon=${lng}&days=7&key=${WB_KEY}`),
-      axios.get(`${PIX_URL}key=${PIX_KEY}&q=${name}&image_type=photo&orientation=horizontal&category=travel&order=popular&per_page=7&pretty=true`),      
-      axios.get(`${PIX_URL}key=${PIX_KEY}&q=${countryName}&image_type=photo&orientation=horizontal&category=travel&order=popular&per_page=7&pretty=true`),
+      axios.get(`${WB_URL}&lat=${lat}&lon=${lng}&days=7&key=${process.env.WB_KEY}`),
+      axios.get(`${PIX_URL}key=${process.env.PIX_KEY}&q=${name}&image_type=photo&orientation=horizontal&category=travel&order=popular&per_page=7&pretty=true`),      
+      axios.get(`${PIX_URL}key=${process.env.PIX_KEY}&q=${countryName}&image_type=photo&orientation=horizontal&category=travel&order=popular&per_page=7&pretty=true`),
     ]);
 
     const pixHits =
